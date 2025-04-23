f9ecfe0289a5f815cec5e705a8afa751bf8bd03b
rpamgi <dipdastoko@gmail.com>
2025-04-23 13:38:38 +0600

    basic project setup commplete

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-const express = require("express");
+import express, { Request, Response } from "express";
+import cors from "cors";
 const app = express();
-const port = 3000;
+//parser
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export default app;
