a00affac0745cc7fc423fe446750aba7f99160aa
rpamgi <dipdastoko@gmail.com>
2025-04-26 13:13:28 +0600

    create-student route added, student inserted successfully into DB

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { StudentRoutes } from "./mdoules/student/student.route";
 const app = express();
 //parser
 app.use(express.json());
 app.use(cors());
+
+// application routes
+app.use("/api/v1/students", StudentRoutes);
 
 const getAController = (req: Request, res: Response) => {
   res.send("Hello World!");
