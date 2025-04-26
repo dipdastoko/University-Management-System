136a9ea1fae7e337c3bf6af1135062c510ca0510
rpamgi <dipdastoko@gmail.com>
2025-04-26 12:11:29 +0600

    route, controller and service added for student module

--- src/mdoules/student/student.model.ts
+++ src/mdoules/student/student.model.ts
@@ -75,4 +75,4 @@ const studentSchema = new Schema<Student>({
   isActive: ["active", "inactive"],
 });
 
-const Student = model<Student>("Student", studentSchema);
+export const StudentModel = model<Student>("Student", studentSchema);
