8044c5e0e72843a8ad3c500c893e9727b704ef53
rpamgi <dipdastoko@gmail.com>
2025-04-26 13:21:10 +0600

    get all students api added, api working successfully

--- src/mdoules/student/student.controller.ts
+++ src/mdoules/student/student.controller.ts
@@ -16,6 +16,20 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+const getAllStudent = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+    res.status(200).json({
+      success: true,
+      message: "All Students retrieved successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const StudentControllers = {
   createStudent,
+  getAllStudent,
 };
