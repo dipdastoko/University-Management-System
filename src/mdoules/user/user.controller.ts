7fb89107d618735fb7a32fd74012e328c1d126ab
rpamgi <dipdastoko@gmail.com>
2025-07-24 11:25:39 +0600

    catchAsync is used in user controller

--- src/mdoules/user/user.controller.ts
+++ src/mdoules/user/user.controller.ts
@@ -1,30 +1,23 @@
-import { RequestHandler } from "express";
 import { UserServices } from "./user.service";
 import sendResponse from "../../app/utils/sendResponse";
 import httpStatus from "http-status";
+import catchAsync from "../../app/utils/catchAsync";
 
-const createStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { password, student: studentData } = req.body;
+const createStudent = catchAsync(async (req, res) => {
+  const { password, student: studentData } = req.body;
 
-    // data validation using zod
-    // const zodParsedData = studentValidationSchema.parse(studentData);
+  // data validation using zod
+  // const zodParsedData = studentValidationSchema.parse(studentData);
 
-    const result = await UserServices.createStudentIntoDB(
-      password,
-      studentData,
-    );
+  const result = await UserServices.createStudentIntoDB(password, studentData);
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "User created successfully",
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "User created successfully",
+    data: result,
+  });
+});
 
 export const UserControllers = {
   createStudent,
