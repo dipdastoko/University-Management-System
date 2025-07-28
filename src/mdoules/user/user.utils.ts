210a403dc12ff8c70ee36a579e0a025839e23a12
rpamgi <dipdastoko@gmail.com>
2025-07-28 16:42:44 +0600

    academic faculty interface, model and validation added

--- src/mdoules/user/user.utils.ts
+++ src/mdoules/user/user.utils.ts
@@ -13,11 +13,24 @@ const findLastStudentId = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payLoad: TAcademicSemster) => {
-  const currentId = (await findLastStudentId()) || 0;
+  let currentId = 0;
+
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const currentSemesterCode = payLoad.code;
+  const currentYear = payLoad.year;
+  if (
+    lastStudentSemesterCode &&
+    lastStudentYear === currentSemesterCode &&
+    currentYear
+  ) {
+    currentId = lastStudentId?.substring(6);
+  }
   let incrementId = (Number(currentId) + 1).toString().padStart(4, "0");
 
   incrementId = `${payLoad.year}${payLoad.code}${incrementId}`;
