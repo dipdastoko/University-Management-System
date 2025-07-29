9d4ba414af69920b12ce095144f5627d705b4367
rpamgi <dipdastoko@gmail.com>
2025-07-29 12:03:01 +0600

    academic faculty validation added

--- src/mdoules/academicFaculty/academicFaculty.validation.ts
+++ src/mdoules/academicFaculty/academicFaculty.validation.ts
@@ -1,14 +1,18 @@
 import { z } from "zod";
 
 const createAcademicFacultyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: "Academic Faculty must be String",
+  body: z.object({
+    name: z.string({
+      invalid_type_error: "Academic Faculty must be String",
+    }),
   }),
 });
 
 const updateAcademicFacultyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: "Academic Faculty must be String",
+  body: z.object({
+    name: z.string({
+      invalid_type_error: "Academic Faculty must be String",
+    }),
   }),
 });
 
