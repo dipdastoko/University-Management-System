c8175b16d1808b2f7cf868e93db42d1a0d5275b7
rpamgi <dipdastoko@gmail.com>
2025-07-30 11:10:54 +0600

    Academic Department services created

---
+++ src/mdoules/academicDepartment/academicDepartment.service.ts
@@ -0,0 +1,32 @@
+import { TAcademicDepartment } from "./academicDepartment.interface";
+import { AcademicDepartment } from "./academicDepratment.model";
+
+const createAcademicDepartmentIntoDB = async (payLoad: TAcademicDepartment) => {
+  const result = await AcademicDepartment.create(payLoad);
+  return result;
+};
+
+const updateAcademicDepartmentIntoDB = async (
+  id: string,
+  payLoad: Partial<TAcademicDepartment>,
+) => {
+  const result = await AcademicDepartment.updateOne({ _id: id }, payLoad);
+  return result;
+};
+
+const getAllAcademicDepartmentsFromDB = async () => {
+  const result = await AcademicDepartment.find();
+  return result;
+};
+
+const getSingleDepartmentFromDB = async (id: string) => {
+  const result = await AcademicDepartment.findById(id);
+  return result;
+};
+
+export const AcademicDepartmentServices = {
+  createAcademicDepartmentIntoDB,
+  updateAcademicDepartmentIntoDB,
+  getAllAcademicDepartmentsFromDB,
+  getSingleDepartmentFromDB,
+};
