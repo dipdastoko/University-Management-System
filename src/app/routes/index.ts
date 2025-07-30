2a5d348544340d8fb583d3f66c4e22163af338f8
rpamgi <dipdastoko@gmail.com>
2025-07-30 12:13:43 +0600

    all routes working properly

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { StudentRoutes } from "../../mdoules/student/student.route";
 import { UserRoutes } from "../../mdoules/user/user.route";
 import { AcademicSemesterRoutes } from "../../mdoules/academicSemster/academicSemester.route";
 import { AcademicFacultyRoutes } from "../../mdoules/academicFaculty/academicFaculty.route";
+import { AcademicDepartmentRouters } from "../../mdoules/academicDepartment/academicDepartmentRoutes";
 const router = express.Router();
 
 const moduleRoutes = [
@@ -21,6 +22,10 @@ const moduleRoutes = [
   {
     path: "/academic-faculty",
     route: AcademicFacultyRoutes,
+  },
+  {
+    path: "/academic-department",
+    route: AcademicDepartmentRouters,
   },
 ];
 
